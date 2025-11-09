dffa1b2c437c9e1b888462bb05ef6ced83c1b389
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-09 17:29:14 +0530

    Create variables.cpp

---
+++ rakesh cpp/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=40;
+    int b=20;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"a/b ="<<a/b<<endl;
+    return 0;
+}
