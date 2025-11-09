cce70654ad0a63aa25db3fd18bf0283aa0648e73
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-09 17:29:05 +0530

    Create print_pattern.cpp

---
+++ rakesh cpp/print_pattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"****\n***\n**\n*\n";
+
+    cout<<"****\n";
+    cout<<"***\n";
+    cout<<"**\n";
+    cout<<"*\n";
+
+
+    return 0;
+}
