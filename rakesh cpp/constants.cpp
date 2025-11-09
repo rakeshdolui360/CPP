e078f2b9000b0a374c2b39e857a7929ec94410cc
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-09 17:30:15 +0530

    Create constants.cpp

---
+++ rakesh cpp/constants.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    const int n=25;
+    const float pi=3.14;
+
+    // not allowed as constant variables must be initialised in same line
+    // const float num;
+    // num=34;
+
+
+
+    return 0;
+}
