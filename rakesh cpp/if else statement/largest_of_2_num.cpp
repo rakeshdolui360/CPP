3fb6e843848354edd507e489c1bf59aaa3696a90
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-12 08:30:07 +0530

    Create largest_of_2_num.cpp

---
+++ rakesh cpp/if else statement/largest_of_2_num.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"enter the value of a ";
+    cin>>a;
+    cout<<"enter the value of b ";
+    cin>>b;  
+    
+    if(a>b){
+        cout<<"a is greater";
+    }
+    else{
+        cout<<"b is greater";
+
+    }
+
+
+    return 0;
+}
