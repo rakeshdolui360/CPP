0e639974929823d1a8804e78441627009776e858
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-12 08:30:18 +0530

    Create odd_even.cpp

---
+++ rakesh cpp/if else statement/odd_even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout<<"enter a integer number ";
+    cin>>num;
+    if(num%2==0){
+        cout<<num<<" is a even number";
+    }
+    else{
+        cout<<num<<" is a odd number";
+
+    }
+
+    return 0;
+}
