52134ca189df3f61262901e9c7319912995c4bf5
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-12 08:29:56 +0530

    Create if_else.cpp

---
+++ rakesh cpp/if else statement/if_else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+
+    cout<<"enter your age ";
+    cin>>age;
+    if(age>=18){
+        cout<<"congrats!! you are eligible for vote"<<endl;
+    }
+    else{
+        cout<<"sorry!! you are not eligible for vote"<<endl;
+
+    }
+
+
+
+
+    return 0;
+}
