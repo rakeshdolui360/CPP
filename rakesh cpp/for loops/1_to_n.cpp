aae01172a91e6302359cdcdaffc9629a067a3f4c
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-13 11:46:31 +0530

    Create 1_to_n.cpp

---
+++ rakesh cpp/for loops/1_to_n.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"enter number ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<i<<endl;
+
+    }
+
+    int num;
+    for(num=0;num<=5;num++){
+        cout<<num<<endl;
+
+    }
+    cout<<"last value of num "<<num<<endl;
+    return 0;
+}
