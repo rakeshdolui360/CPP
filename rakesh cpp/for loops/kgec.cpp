ac39675e38f25cff849e8aea0eb51e51e0555d8e
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-13 11:46:50 +0530

    Create kgec.cpp

---
+++ rakesh cpp/for loops/kgec.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"enter how many times to print ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"kgec"<<endl;
+
+    }
+
+    return 0;
+}
