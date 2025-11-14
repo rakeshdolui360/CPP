019535db5d49dc775655bfed384f926413d65fe1
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-14 20:18:15 +0530

    Create 1_to_n.cpp

---
+++ rakesh cpp/do while loop/1_to_n.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter a number ";
+    cin>>n;
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+
+
+    }while(i<=n);
+
+
+    return 0;
+}
