fd323e0a2561a9fa34b97ceea73dd12a81ea3694
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-11-18 12:55:17 +0530

    Create hello_functions.cpp

---
+++ rakesh cpp/functions/hello_functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void sayhello(){
+    cout<<"hello rakesh \n";
+}
+
+int main() {
+    sayhello();
+    sayhello();
+    sayhello();
+    sayhello();
+
+    return 0;
+}
+
+
